d1b5e2037002f1920239d7014453760ee73cf044
Hwanjin <hwanjin.lee174@gmail.com>
2022-08-26 14:46:16 -0700

    apos

--- components/About.jsx
+++ components/About.jsx
@@ -34,10 +34,10 @@ const About = ({ onComment }) => {
           I&apos;m a front-end focused web developer with visual graphic
           experiences. My interest in web triggered my dev journey starting from
           being a hobby outside work but I loved it so much that it quickly
-          turned into a career transition! I think it's amazing to see how fast
-          web development tool/stack are advancing to make web more useful and
-          powerful. With libraries like React, I love to make components that
-          are useful, appealing, and easy to use.
+          turned into a career transition! I think it&apos;s amazing to see how
+          fast web development tool/stack are advancing to make web more useful
+          and powerful. With libraries like React, I love to make components
+          that are useful, appealing, and easy to use.
         </div>
         <Likes onComment={onComment} />
       </div>
