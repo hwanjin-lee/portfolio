bc23f74b5ab43e02ac7a03fbef0ab08ae8bcb931
Hwanjin <hwanjin.lee174@gmail.com>
2022-10-09 12:47:16 -0700

    navbar logo now clickable

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -3,12 +3,15 @@ import React from "react";
 import styles from "../styles/components/Navbar.module.css";
 import ScrollHighlightNabbar from "../components/ScrollHighlightNabbar";
 import DarkMode from "./DarkMode";
+import Link from "next/link";
 
 const NavBar = ({ onToggle, darkTheme, navHeader }) => {
   return (
     <div className={styles.container}>
       <div className={styles.logo}>
-        <Image src="/img/logo.png" alt="" width="40px" height="40px" />
+        <Link href="/">
+          <Image src="/img/logo.png" alt="" width="40px" height="40px" />
+        </Link>
       </div>
       <ScrollHighlightNabbar navHeader={navHeader} />
       <DarkMode darkTheme={darkTheme} onToggle={onToggle} />
