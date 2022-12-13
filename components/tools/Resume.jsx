dcc9500ccaa81cd263d578d1a205c7539c11b55b
Hwanjin <hwanjin.lee174@gmail.com>
2022-12-13 13:05:57 -0800

    cv rename

--- components/tools/Resume.jsx
+++ components/tools/Resume.jsx
@@ -6,11 +6,7 @@ import styles from "../../styles/components/Resume.module.css";
 const Resume = () => {
   return (
     <div className="box">
-      <a
-        className={styles.container}
-        href="/CVPortfolioVer.pdf"
-        target="_blank"
-      >
+      <a className={styles.container} href="/HwanjinLeeCV.pdf" target="_blank">
         <div className={styles.text}>View Resume</div>
         <FontAwesomeIcon icon={faDownload} className={styles.icons} />
       </a>
