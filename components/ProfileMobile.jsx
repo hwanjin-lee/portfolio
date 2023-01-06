f9306925543ff8d22548b8c5bae4eab9b4320966
Hwanjin <hwanjin.lee174@gmail.com>
2023-01-06 16:56:12 -0700

    resume added on mobile

--- components/ProfileMobile.jsx
+++ components/ProfileMobile.jsx
@@ -5,53 +5,65 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEnvelope, faLocationDot } from "@fortawesome/free-solid-svg-icons";
 import ContactLinks from "./ContactLinks";
 import { mailTo } from "./tools/MailTo";
+import Resume from "./tools/Resume";
 
 const ProfileMobile = () => {
   return (
-    <div className={styles.container}>
-      <div className={styles.backgroundImg}></div>
-      <div className={styles.contents}>
-        <div className={styles.contactCard}>
-          <div className={styles.profileImg}>
-            <Image src="/img/myself.jpg" alt="" width="200px" height="200px" />
-          </div>
-          <div className={styles.intro}>
-            <div className={styles.name}>Hwanjin Lee</div>
-            <div className={styles.contact}>
-              <div
-                className="email"
-                onClick={() => {
-                  mailTo("hwanjin.lee174@gmail.com");
-                }}
-              >
-                <FontAwesomeIcon icon={faEnvelope} className={styles.icon} />
-                hwanjin.lee174@gmail.com
+    <div>
+      <div className={styles.container}>
+        <div className={styles.backgroundImg}></div>
+        <div className={styles.contents}>
+          <div className={styles.contactCard}>
+            <div className={styles.profileImg}>
+              <Image
+                src="/img/myself.jpg"
+                alt=""
+                width="200px"
+                height="200px"
+              />
+            </div>
+            <div className={styles.intro}>
+              <div className={styles.name}>Hwanjin Lee</div>
+              <div className={styles.contact}>
+                <div
+                  className="email"
+                  onClick={() => {
+                    mailTo("hwanjin.lee174@gmail.com");
+                  }}
+                >
+                  <FontAwesomeIcon icon={faEnvelope} className={styles.icon} />
+                  hwanjin.lee174@gmail.com
+                </div>
+                <div>
+                  <FontAwesomeIcon
+                    icon={faLocationDot}
+                    className={styles.icon}
+                  />
+                  San-Diego, CA
+                </div>
               </div>
-              <div>
-                <FontAwesomeIcon icon={faLocationDot} className={styles.icon} />
-                San-Diego, CA
+              <div className={styles.quote}>
+                <div className={styles.quoteWidth}>
+                  &quot;I&apos;m a developer that loves to build, learn, and
+                  find new opportunities to make things come to life!&quot;
+                </div>
               </div>
             </div>
-            <div className={styles.quote}>
-              <div className={styles.quoteWidth}>
-                &quot;I&apos;m a developer that loves to build, learn, and find
-                new opportunities to make things come to life!&quot;
-              </div>
-            </div>
           </div>
+          <hr />
+          <div className={styles.interests}>
+            <div className={styles.interestBox}>#road trips</div>
+            <div className={styles.interestBox}>#cats</div>
+            <div className={styles.interestBox}>#snowboarding</div>
+            <div className={styles.interestBox}>#surfing</div>
+            <div className={styles.interestBox}>#hiking</div>
+            <div className={styles.interestBox}>#gunpla</div>
+          </div>
+          <hr />
+          <ContactLinks />
         </div>
-        <hr />
-        <div className={styles.interests}>
-          <div className={styles.interestBox}>#road trips</div>
-          <div className={styles.interestBox}>#cats</div>
-          <div className={styles.interestBox}>#snowboarding</div>
-          <div className={styles.interestBox}>#surfing</div>
-          <div className={styles.interestBox}>#hiking</div>
-          <div className={styles.interestBox}>#gunpla</div>
-        </div>
-        <hr />
-        <ContactLinks />
       </div>
+      <Resume />
     </div>
   );
 };
